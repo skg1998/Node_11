4523a3c776ab6768fdc48ce474dad68e2cac6288
skg1998 <sahilkg1998@gmail.com>
2021-05-03 14:36:34 +0530

    fix(database-connection): connect to DB

--- node-docker/src/index.js
+++ node-docker/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const dbConnect = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+dbConnect();
+
 app.get('/', (req, res) => {
-    res.send("<h1> Hello world </h1>");
+    res.send("<h1> Hello world !!!</h1>");
 })
 
 app.listen(port, () => {
