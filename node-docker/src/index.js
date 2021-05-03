1c79cddf0ee64dafc9592194a6e9553dd1d765f0
skg1998 <sahilkg1998@gmail.com>
2021-05-03 15:33:18 +0530

    fix(postroutes): connect post routes to app

--- node-docker/src/index.js
+++ node-docker/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dbConnect = require('./config/db');
+const PostRouter = require('./routes/postRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,8 @@ dbConnect();
 app.get('/', (req, res) => {
     res.send("<h1> Hello world !!!</h1>");
 })
+
+app.use('/api/posts', PostRouter);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
