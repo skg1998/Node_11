c81f31447cb44df943a1cc3176e4a93b3cae299e
skg1998 <sahilkg1998@gmail.com>
2021-05-04 12:38:36 +0530

    fix(body-parser): fix body parser

--- node-docker/src/index.js
+++ node-docker/src/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const dbConnect = require('./config/db');
 const PostRouter = require('./routes/postRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+//call db 
 dbConnect();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send("<h1> Hello world !!!</h1>");
 })
 
-app.use('/api/posts', PostRouter);
+app.use('/api/v1/posts', PostRouter);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
