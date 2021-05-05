02f29bb855dd5dd843c23166a37b86e7dee0e846
skg1998 <sahilkg1998@gmail.com>
2021-05-05 15:40:25 +0530

    feat(session): create session

--- node-docker/src/index.js
+++ node-docker/src/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dbConnect = require('./config/db');
+const redis = require('redis');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const { REDIS_URL, REDIS_PORT, SESSION_SECRET } = require('./config/config');
+
+const redisClient = redis.createClient({
+    host: REDIS_URL,
+    port: REDIS_PORT
+})
+
 const PostRouter = require('./routes/postRoutes');
 const UserRouter = require('./routes/userRoutes');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,6 +26,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(express.json())
+app.use(session({
+    store: new RedisStore({
+        client: redisClient
+    }),
+    secret: SESSION_SECRET,
+    cookie: {
+        secure: false,
+        resave: false,
+        saveUninitialized: false,
+        httpOnly: true,
+        maxAge: 30000
+    }
+}))
 
 app.get('/', (req, res) => {
     res.send("<h1> Hello world !!!</h1>");
