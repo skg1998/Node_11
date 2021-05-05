9fe8c28d42fdab88286c8bd3cc7bf47385d4cc9d
skg1998 <sahilkg1998@gmail.com>
2021-05-05 15:41:21 +0530

    fix(middleware): fix auth middleware

--- node-docker/src/routes/postRoutes.js
+++ node-docker/src/routes/postRoutes.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const postController = require('../controller/postController');
+const { hasAuthorization } = require('../middleware/authMiddleware')
 
 const router = express.Router();
 
 router
     .route("/")
     .get(postController.getAllPost)
-    .post(postController.createPost);
+    .post(hasAuthorization, postController.createPost);
+
 router
     .route("/:id")
     .get(postController.getOnePost)
-    .patch(postController.updatePost)
-    .delete(postController.deletePost)
+    .patch(hasAuthorization, postController.updatePost)
+    .delete(hasAuthorization, postController.deletePost)
 
 
 module.exports = router;
