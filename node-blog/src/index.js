91909a590ea15530e067ce99c8d48b44f0153bc0
skg1998 <sahilkg1998@gmail.com>
2021-05-12 10:42:51 +0530

    feat(middleware): add logger middleware

--- node-blog/src/index.js
+++ node-blog/src/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+//Router files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
 //Load env var
-dotenv.config({ path: './config/config.env' })
+dotenv.config()
 
 const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'succes',
+        message: 'ok'
+    })
+})
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//Mount Routes
+app.use('/api/v1/bootcamps', bootcamps)
 
 app.listen(PORT, () => {
     console.log(`server is listen in ${process.env.NODE_ENV} on ${PORT} `);
