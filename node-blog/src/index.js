6070945abb57100ac553bd364e174339ea2eb71e
skg1998 <sahilkg1998@gmail.com>
2021-05-13 11:44:44 +0530

    feat(database_connected): connect database

--- node-blog/src/index.js
+++ node-blog/src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const color = require('colors');
+const connectDB = require('./config/db');
+
+//connect Database
+connectDB();
 
 //Router files
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+//express json
+app.use(express.json())
 
 //Load env var
 dotenv.config()
@@ -28,5 +36,12 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps)
 
 app.listen(PORT, () => {
-    console.log(`server is listen in ${process.env.NODE_ENV} on ${PORT} `);
+    console.log(`server is listen in ${process.env.NODE_ENV} on ${PORT} `.yellow.bold);
 });
+
+//Handle unHandled Promises rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message} `.red);
+    //close server and exit process
+    app.close(() => process.exit())
+})
