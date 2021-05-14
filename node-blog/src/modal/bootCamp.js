f9fc2bf56fb9734d7de5df7a095adbf860565b98
skg1998 <sahilkg1998@gmail.com>
2021-05-14 12:50:41 +0530

    feat(slugify): use slugify to generate slug

--- node-blog/src/modal/bootCamp.js
+++ node-blog/src/modal/bootCamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootCampSchema = new mongoose.Schema({
     name: {
@@ -84,6 +85,12 @@ const BootCampSchema = new mongoose.Schema({
         type: Date,
         deafult: Date.now
     },
+})
+
+//create bootcamp slug from the name
+BootCampSchema.pre('save', (next) => {
+    this.slug = slugify(this.name, { lower: true })
+    next();
 })
 
 module.exports = mongoose.model('BootCamp', BootCampSchema);
