2714c400cc72081e34a2511b735bb21cf04fe2d6
skg1998 <sahilkg1998@gmail.com>
2021-05-17 17:39:27 +0530

    feat(getBootcampsByRadius): add a routes to get bootcamp by radius

--- node-blog/src/routes/bootcamps.js
+++ node-blog/src/routes/bootcamps.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getallBootcamps, getBootcamp, updateBootcamp, createBootcamp, deleteBootcamp } = require('../controller/bootcamps');
+const { getallBootcamps, getBootcamp, updateBootcamp, createBootcamp, deleteBootcamp, getBootcampsByRadius } = require('../controller/bootcamps');
 
 router.route('/').get(getallBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
+router.route('/:zipcode/:distance').get(getBootcampsByRadius);
 
 module.exports = router
