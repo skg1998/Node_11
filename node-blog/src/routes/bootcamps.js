73c425692ec1d0159b04413fff15403245826f92
skg1998 <sahilkg1998@gmail.com>
2021-05-19 19:27:25 +0530

    feat(middleware): add filter routes

--- node-blog/src/routes/bootcamps.js
+++ node-blog/src/routes/bootcamps.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const advancedResult = require('../middleware/advancedResult');
+const BootCamp = require('../model/bootCamp');
 const { getallBootcamps, getBootcamp, updateBootcamp, createBootcamp, deleteBootcamp, getBootcampsByRadius, bootcampPhotoUpload } = require('../controller/bootcamps');
 
 //Includes other resource routers
@@ -8,7 +10,7 @@ const courseRouter = require('./courses')
 //Re-route in other source routers
 router.use('/:bootcampId/courses', courseRouter)
 
-router.route('/').get(getallBootcamps).post(createBootcamp);
+router.route('/').get(advancedResult(BootCamp, 'courses'), getallBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 router.route('/radius/:zipcode/:distance').get(getBootcampsByRadius);
 router.route('/:id/photo').put(bootcampPhotoUpload)
--- node-blog/src/routes/courses.js
+++ node-blog/src/routes/courses.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
+
+const Course = require('../model/course');
+const advancedResult = require('../middleware/advancedResult');
 const { getCourse, getSingleCourse, addCourse, updateCourse, deleteCourse } = require('../controller/courses');
 
-router.route('/').get(getCourse).post(addCourse)
+router.route('/').get(advancedResult(Course, {
+    path: 'bootcamp',
+    select: 'name description'
+}), getCourse).post(addCourse)
 router.route('/:id').get(getSingleCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router
