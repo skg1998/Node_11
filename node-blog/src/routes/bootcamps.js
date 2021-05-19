5d4f7b637a120f6503d34729b4c419ce7ff24281
skg1998 <sahilkg1998@gmail.com>
2021-05-19 18:12:02 +0530

    feat(add_more_route): add photoupload bootcampbyradius

--- node-blog/src/routes/bootcamps.js
+++ node-blog/src/routes/bootcamps.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { getallBootcamps, getBootcamp, updateBootcamp, createBootcamp, deleteBootcamp, getBootcampsByRadius } = require('../controller/bootcamps');
+const { getallBootcamps, getBootcamp, updateBootcamp, createBootcamp, deleteBootcamp, getBootcampsByRadius, bootcampPhotoUpload } = require('../controller/bootcamps');
+
+//Includes other resource routers
+const courseRouter = require('./courses')
+
+//Re-route in other source routers
+router.use('/:bootcampId/courses', courseRouter)
 
 router.route('/').get(getallBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
-router.route('/:zipcode/:distance').get(getBootcampsByRadius);
+router.route('/radius/:zipcode/:distance').get(getBootcampsByRadius);
+router.route('/:id/photo').put(bootcampPhotoUpload)
 
 module.exports = router
