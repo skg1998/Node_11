c419e4b62e4b2f526fe1dd846ce2d7c13fe306e8
skg1998 <sahilkg1998@gmail.com>
2021-05-20 11:09:39 +0530

    feat(auth_model): create auth model and hash password using bcrypt

---
+++ node-blog/src/model/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add email"],
+        unique: true,
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "please add a valid email"]
+    },
+    role: {
+        type: String,
+        enum: ['user', 'publisher'],
+        default: 'user',
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a passwoord'],
+        minlength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+//Encrypt Password using Bcrypt
+UserSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt);
+})
+
+//Sign jWT and return 
+UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    })
+}
+
+module.exports = mongoose.model('User', UserSchema)
