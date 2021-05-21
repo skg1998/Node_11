31cfd2ed2a89ab9c9f33e49362911bd72fbe1925
skg1998 <sahilkg1998@gmail.com>
2021-05-21 13:16:26 +0530

    feat(resetPassword): add a forgotPassword route to reset password

--- node-blog/src/routes/auth.js
+++ node-blog/src/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router();
 const { protect } = require('../middleware/hasAuth');
-const { register, login, getMe } = require('../controller/auth');
+const { register, login, getMe, forgotPassword } = require('../controller/auth');
 
 router.route('/register').post(register)
 router.route('/login').post(login)
 router.route('/me').get(protect, getMe)
+router.route('/forgotPassword').post(forgotPassword);
 
 module.exports = router
