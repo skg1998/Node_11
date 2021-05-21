93f97170281738b937cfb3e406e4569d1b872fb9
skg1998 <sahilkg1998@gmail.com>
2021-05-21 11:30:51 +0530

    feat(middleware): create a hasAuth middleware

---
+++ node-blog/src/middleware/hasAuth.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+const User = require('../model/User');
+const ErrorResponse = require('../util/errorResponse');
+
+exports.protect = async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(" ")[1]
+    }
+
+    //Make sure token exist
+    if (!token) {
+        return next(new ErrorResponse('Not Authorized to access this route', 401))
+    }
+
+    try {
+        //verify token
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decode)
+        req.user = await User.find(decode.id)
+    } catch (err) {
+        return next(new ErrorResponse('Not Authorized to access this route', 401))
+    }
+}
+
+//Grant access to role
+exports.authorize = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorResponse(`User role ${req.user.role} is Not Authorized to access this route`, 403))
+        }
+        next();
+    }
+}
