18b40990a7924e8b62203ef6ff7c51d469ec7478
skg1998 <sahilkg1998@gmail.com>
2021-05-23 10:24:42 +0530

    feat(review): add reviews CRUD functionality

---
+++ node-blog/src/controller/Reviews.js
@@ -0,0 +1,143 @@
+const ErrorResponse = require('../util/errorResponse');
+const Review = require('../model/Review');
+const Bootcamp = require('../model/bootCamp');
+
+// @desc      Get reviews
+// @route     GET /api/v1/reviews
+// @route     GET /api/v1/bootcamps/:bootcampId/reviews
+// @access    Public
+exports.getReviews = async (req, res, next) => {
+    try {
+        if (req.params.bootcampId) {
+            const reviews = await Review.find({ bootcamp: req.params.bootcampId });
+
+            return res.status(200).json({
+                success: true,
+                count: reviews.length,
+                data: reviews
+            });
+        } else {
+            res.status(200).json(res.advancedResults);
+        }
+    } catch (err) {
+        next(err);
+    }
+}
+
+// @desc      Get single review
+// @route     GET /api/v1/reviews/:id
+// @access    Public
+exports.getReview = async (req, res, next) => {
+    try {
+        const review = await Review.findById(req.params.id).populate({
+            path: 'bootcamp',
+            select: 'name description'
+        });
+
+        if (!review) {
+            return next(
+                new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
+            );
+        }
+
+        res.status(200).json({
+            success: true,
+            data: review
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+// @desc      Add review
+// @route     POST /api/v1/bootcamps/:bootcampId/reviews
+// @access    Private
+exports.addReview = async (req, res, next) => {
+    try {
+        req.body.bootcamp = req.params.bootcampId;
+        req.body.user = req.user.id;
+
+        const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+        if (!bootcamp) {
+            return next(
+                new ErrorResponse(
+                    `No bootcamp with the id of ${req.params.bootcampId}`,
+                    404
+                )
+            );
+        }
+
+        const review = await Review.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            data: review
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+// @desc      Update review
+// @route     PUT /api/v1/reviews/:id
+// @access    Private
+exports.updateReview = async (req, res, next) => {
+    try {
+        let review = await Review.findById(req.params.id);
+
+        if (!review) {
+            return next(
+                new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+            );
+        }
+
+        // Make sure review belongs to user or user is admin
+        if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return next(new ErrorResponse(`Not authorized to update review`, 401));
+        }
+
+        review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        review.save();
+
+        res.status(200).json({
+            success: true,
+            data: review
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+// @desc      Delete review
+// @route     DELETE /api/v1/reviews/:id
+// @access    Private
+exports.deleteReview = async (req, res, next) => {
+    try {
+        const review = await Review.findById(req.params.id);
+
+        if (!review) {
+            return next(
+                new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+            );
+        }
+
+        // Make sure review belongs to user or user is admin
+        if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return next(new ErrorResponse(`Not authorized to delete review`, 401));
+        }
+
+        await review.remove();
+
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (err) {
+        next(err);
+    }
+}
